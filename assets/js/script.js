8bf35763d6e88ad7da4870204c3c7ae8f3777dfe
Ahmad Bilal <ahmad.bilal.chohan@gmail.com>
2024-09-07 04:49:53 +0500

    9th commit, changed the navbar a bit along with some responsive elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,87 +8,86 @@ window.addEventListener("load", function () {
 // preloader script ends here.........
 
 // switch for setting
-function settingtoggle(){
+function settingtoggle() {
   document.getElementById("setting-container").classList.toggle('settingactivate');
   document.getElementById("visualmodetogglebuttoncontainer").classList.toggle('visualmodeshow');
 }
 
-function visualmode(){
+function visualmode() {
   document.body.classList.toggle('light-mode');
   var elements = document.querySelectorAll('.needtobeinvert');
-  elements.forEach(function(element) {
-      element.classList.toggle('invertapplied');
+  elements.forEach(function (element) {
+    element.classList.toggle('invertapplied');
   });
 
 
 }
 let emptyArea = document.getElementById("emptyarea");
- let mobileTogglemenu = document.getElementById("mobiletogglemenu");
+let mobileTogglemenu = document.getElementById("mobiletogglemenu");
 // toggle menu by clicking on hamburger
 function hamburgerMenu() {
-    document.body.classList.toggle("stopscrolling");
- document.getElementById("mobiletogglemenu").classList.toggle("show-toggle-menu");
-document.getElementById("burger-bar1").classList.toggle("hamburger-animation1");
-document.getElementById("burger-bar2").classList.toggle("hamburger-animation2");
-document.getElementById("burger-bar3").classList.toggle("hamburger-animation3");
+  document.body.classList.toggle("stopscrolling");
+  document.getElementById("mobiletogglemenu").classList.toggle("show-toggle-menu");
+  document.getElementById("burger-bar1").classList.toggle("hamburger-animation1");
+  document.getElementById("burger-bar2").classList.toggle("hamburger-animation2");
+  document.getElementById("burger-bar3").classList.toggle("hamburger-animation3");
 }
 // close mobile toggle menu by clicking on LI
-function hidemenubyli(){
-document.body.classList.toggle("stopscrolling");
-document.getElementById("mobiletogglemenu").classList.remove("show-toggle-menu");
-document.getElementById("burger-bar1").classList.remove("hamburger-animation1");
-document.getElementById("burger-bar2").classList.remove("hamburger-animation2");
-document.getElementById("burger-bar3").classList.remove("hamburger-animation3");
+function hidemenubyli() {
+  document.body.classList.toggle("stopscrolling");
+  document.getElementById("mobiletogglemenu").classList.remove("show-toggle-menu");
+  document.getElementById("burger-bar1").classList.remove("hamburger-animation1");
+  document.getElementById("burger-bar2").classList.remove("hamburger-animation2");
+  document.getElementById("burger-bar3").classList.remove("hamburger-animation3");
 }
 
 const sections = document.querySelectorAll('section');
 const navLi = document.querySelectorAll('.navbar .navbar-tabs .navbar-tabs-ul li');
 const mobilenavLi = document.querySelectorAll('.mobiletogglemenu .mobile-navbar-tabs-ul li');
 
-window.addEventListener('scroll', ()=>{
+window.addEventListener('scroll', () => {
   let current = "";
   sections.forEach(section => {
     const sectionTop = section.offsetTop;
     const sectionHeight = section.clientHeight;
-    if(pageYOffset >= (sectionTop - 200)){
+    if (pageYOffset >= (sectionTop - 200)) {
       current = section.getAttribute('id');
     }
   })
 
-  mobilenavLi.forEach( li => {
+  mobilenavLi.forEach(li => {
     li.classList.remove('activeThismobiletab');
-    if(li.classList.contains(current)){
+    if (li.classList.contains(current)) {
       li.classList.add('activeThismobiletab')
     }
   })
-navLi.forEach( li => {
-  li.classList.remove('activeThistab');
-  if(li.classList.contains(current)){
-    li.classList.add('activeThistab')
-  }
-})
+  navLi.forEach(li => {
+    li.classList.remove('activeThistab');
+    if (li.classList.contains(current)) {
+      li.classList.add('activeThistab')
+    }
+  })
 })
 console.log('%c Designed and Developed by Ahmad Bilal ', 'background-image: linear-gradient(90deg,#8000ff,#6bc5f8); color: white;font-weight:900;font-size:1rem; padding:20px;');
 
 
 
 let mybutton = document.getElementById("backtotopbutton");
-window.onscroll = function(){
+window.onscroll = function () {
   scrollFunction()
 };
 
-function scrollFunction(){
-  if(document.body.scrollTop > 400 || document.documentElement.scrollTop > 400)
-  {
+function scrollFunction() {
+  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
     mybutton.style.display = "block";
   }
-   else{
-      mybutton.style.display = "none";
-     
-      }
+  else {
+    mybutton.style.display = "none";
+
+  }
 }
 
-function scrolltoTopfunction(){
+function scrolltoTopfunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
@@ -96,9 +95,9 @@ function scrolltoTopfunction(){
 //   e.preventDefault();
 // }, false);
 
-document.addEventListener("contextmenu", function(e){
+document.addEventListener("contextmenu", function (e) {
   if (e.target.nodeName === "IMG") {
-      e.preventDefault();
+    e.preventDefault();
   }
 }, false);
 
@@ -127,27 +126,27 @@ let fracYValue = 0;
 let mouseXRange = mouseXEndPoint - mouseXStartPoint;
 
 const mouseMove = (event) => {
-    currentXPosition = event.clientX - mouseXStartPoint;
-    fracXValue = currentXPosition / mouseXRange;
+  currentXPosition = event.clientX - mouseXStartPoint;
+  fracXValue = currentXPosition / mouseXRange;
 
-    currentYPosition = event.clientY;
-    fracYValue = currentYPosition / mouseYEndPoint;
- 
-    // footer
-    let pupilXCurrrentPosition = pupilStartPoint + (fracXValue * pupilRangeX);
-    let pupilYCurrrentPosition = pupilStartPoint + (fracYValue * pupilRangeY);
+  currentYPosition = event.clientY;
+  fracYValue = currentYPosition / mouseYEndPoint;
 
-    // footer
-    pupilsArr.forEach((curPupil) => {
-      curPupil.style.transform= `translate(${pupilXCurrrentPosition}px, ${pupilYCurrrentPosition}px)`;
+  // footer
+  let pupilXCurrrentPosition = pupilStartPoint + (fracXValue * pupilRangeX);
+  let pupilYCurrrentPosition = pupilStartPoint + (fracYValue * pupilRangeY);
+
+  // footer
+  pupilsArr.forEach((curPupil) => {
+    curPupil.style.transform = `translate(${pupilXCurrrentPosition}px, ${pupilYCurrrentPosition}px)`;
   })
 
 }
 
 const windowResize = (event) => {
-    mouseXEndPoint = window.innerWidth;
-    mouseYEndPoint = window.innerHeight;
-    mouseXRange = mouseXEndPoint - mouseXStartPoint;
+  mouseXEndPoint = window.innerWidth;
+  mouseYEndPoint = window.innerHeight;
+  mouseXRange = mouseXEndPoint - mouseXStartPoint;
 }
 
 
